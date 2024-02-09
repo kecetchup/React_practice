ae9fe071575de74d36d3bf66aac66ff127288cc7
kecetchup <yeonchanho0927@naver.com>
2024-02-09 16:56:10 +0900

    새로운 부분 수정

--- src/App.js
+++ src/App.js
@@ -76,6 +76,9 @@ function App() {
       const newTopics = [...topics]
       newTopics.push(newTopic);
       setTopics(newTopics);
+      setMode('READ');
+      setId(nextId);
+      setNextId(nextId+1);
     }}></Create>
   }
 
